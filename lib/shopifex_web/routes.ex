feacb3d621477b64f26dd2a572a0c551f779e0cc
Brian Sage <brian@sagedigital.com>
2026-03-03 15:45:58 -0700

    Remove put_root_layout from :shopifex_browser - ShopifexWeb.Layouts does not exist

--- lib/shopifex_web/routes.ex
+++ lib/shopifex_web/routes.ex
@@ -30,7 +30,6 @@ defmodule ShopifexWeb.Routes do
         plug(:accepts, ["html"])
         plug(:fetch_session)
         plug(:fetch_live_flash)
-        plug(:put_root_layout, html: {ShopifexWeb.Layouts, :root})
         plug(:protect_from_forgery)
         plug(:put_secure_browser_headers)
 
